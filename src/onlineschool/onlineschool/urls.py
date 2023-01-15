7df638e274abb8d596d388164b5e77b8d43c0757
миша сабадаш <misasabadas@mishas-MacBook-Air.local>
2023-01-16 02:56:50 +0700

    start

--- src/onlineschool/onlineschool/urls.py
+++ src/onlineschool/onlineschool/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from onlineschool import views 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('signup/',views.signup,name='signup'),
+    path('signin/',views.signup,name='signin'),
+
 ]
--- src/onlineschool/users/views.py
+++ src/onlineschool/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+def signup(request):
+    return render(request,'onlineschool/signup.html')
+def signin(request):
+    return render(request,'onlineschool/signin.html')
 # Create your views here.
