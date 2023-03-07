d3e2caa82ceb7c76c7aee191d1cc1724c91d8cca
миша сабадаш <misasabadas@mishas-MacBook-Air.local>
2023-03-08 01:30:46 +0300

    makemain

--- src/onlineschool/onlineschool/urls.py
+++ src/onlineschool/onlineschool/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users import views 
+from main import views 
 from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
